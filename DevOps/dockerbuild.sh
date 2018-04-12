b0eb4d0de143752e10aa84758cba24e9d82ce6ce
Shwshank <shwshank3@gmail.com>
2018-04-12 15:43:00 +0530

    DevOps dockerbuild commented 3 lines

--- DevOps/dockerbuild.sh
+++ DevOps/dockerbuild.sh
@@ -13,15 +13,13 @@ REPONAME="collect-frontend-pipeline"
 
 git clone $GITHUB_LINK
 
-mv /var/lib/jenkins/workspace/collect-frontend/dist/ /home/ubuntu/dist_backups/collect/dist_$TIMESTAMP
-
 mv /var/lib/jenkins/workspace/collect-frontend-pipeline/dist /var/lib/jenkins/workspace/collect-frontend/
 
-docker stop $DOCKERNAME
-
-docker rm $DOCKERNAME
-
-docker rmi $IMAGENAME
+# docker stop $DOCKERNAME
+#
+# docker rm $DOCKERNAME
+#
+# docker rmi $IMAGENAME
 
 docker build -t $IMAGENAME /var/lib/jenkins/workspace/collect-frontend/.
 
