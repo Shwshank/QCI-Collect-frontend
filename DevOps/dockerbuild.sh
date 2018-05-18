bd747a8642c5912667bd1647b34ea0247c5454c3
Shwshank <shwshank3@gmail.com>
2018-05-18 16:44:09 +0530

    corrected devops file

--- DevOps/dockerbuild.sh
+++ DevOps/dockerbuild.sh
@@ -10,17 +10,13 @@ IMAGENAME="qci-collect-front"
 
 REPONAME="collect-frontend-pipeline"
 
-
-git clone $GITHUB_LINK
-
+# git clone $GITHUB_LINK
+chgrp -R docker /var/lib/jenkins/workspace/collect-frontend-pipeline/dist
+mv /var/lib/jenkins/workspace/collect-frontend/dist/ /home/ubuntu/dist_backups/collect/dist_$TIMESTAMP
 mv /var/lib/jenkins/workspace/collect-frontend-pipeline/dist /var/lib/jenkins/workspace/collect-frontend/
-
-# docker stop $DOCKERNAME
-#
-# docker rm $DOCKERNAME
-#
-# docker rmi $IMAGENAME
-
+rm -rf $REPONAME
+docker stop $DOCKERNAME
+docker rm $DOCKERNAME
+docker rmi $IMAGENAME
 docker build -t $IMAGENAME /var/lib/jenkins/workspace/collect-frontend/.
-
 docker run -dt --name $DOCKERNAME $IMAGENAME
