797514a0d926dbdb01d0ac5309a0931cd1e46510
Shwshank <shwshank3@gmail.com>
2018-05-25 10:55:32 +0530

    updated api

--- src/app/service/APIService.ts
+++ src/app/service/APIService.ts
@@ -4,9 +4,9 @@ import { EventEmitter, Injectable } from '@angular/core';
 @Injectable()
 export class APIService {
 
-  projectURL: string = 'http://192.168.15.187:8000';
+  // projectURL: string = 'http://192.168.15.187:8000';
   responseSocketURL: string = 'ws://192.168.15.187:8000';
-  // projectURL: string = 'https://qcitech.org:8083';
+  projectURL: string = 'https://qcitech.org:8083';
 
   userID : any = "";
 
